9793abaf843197d03217caf5d2c40ac1e26a38fa
Ashish <ashish.gupta@addverb.in>
2021-05-03 12:37:09 +0530

    initial commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotesComponent } from './Components/notes/notes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'notes',
+    component: NotesComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/notes',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
